30452f96f107f9ad351ee675163d706e50596fbe
saikasyap123 <saikasyap99@gmail.com>
2022-04-26 20:00:24 +0530

    day 4 completed

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -6,16 +6,42 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Center(
-      child: Container(
-        child: Text(
-          "Login Here!!!",
-          style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent),
-        ),
-      ),
-    ));
+        color: Colors.white,
+        child: ListView(children: <Widget>[
+          Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              "Welcome",
+              style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(children: [
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Username", labelText: "User Name")),
+              TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password")),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () => {print("Button Pressed")},
+                  child: Text("Login"))
+            ]),
+          )
+        ]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'homePage.dart';
 import "loginPage.dart";
+import "package:google_fonts/google_fonts.dart";
 
 void main() {
   runApp(MyApp());
@@ -11,8 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: "/home",
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
